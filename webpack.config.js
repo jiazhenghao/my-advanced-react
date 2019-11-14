7cb6aaf37fb0188311c8ee773dadb08ce4d5a878
Zhenghao Jia <cherie-xia@hotmail.com>
2019-11-14 22:36:11 +0800

    async

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,9 @@
 const path = require("path");
-// const HtmlWebpackPlugin = require("html-webpack-plugin"); //installed via npm
-// const webpack = require("webpack"); //to access built-in plugins
 
 module.exports = {
   mode: "development",
   //entry: ["@babel/polyfill", "./lib/components/index.js"],
-  entry: "./lib/components/index.js",
+  entry: "./lib/renders/dom.js",
   output: {
     path: path.resolve(__dirname, "public"),
     filename: "bundle.js"
@@ -13,6 +11,8 @@ module.exports = {
   module: {
     rules: [{ test: /\.js$/, exclude: /node_modules/, use: "babel-loader" }]
     //rules: [{ test: /\.js$/, use: "babel-loader" }]
+  },
+  resolve: {
+    modules: [path.resolve("./lib"), path.resolve("./node_modules")]
   }
-  //plugins: [new HtmlWebpackPlugin({ template: "./src/index.html" })]
 };
